92bd86d348decd3b18bee9c0b484494b9ab51ec7
yamancan <yamancandev@gmail.com>
2025-11-22 18:45:11 -0300

    feat(design-system): update index.css and tailwind.config.js with design tokens

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -5,7 +5,18 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      backdropBlur: {
+        '7': '7px',
+        '7.5': '7.5px',
+        '17.5': '17.5px',
+      },
+      backgroundBlendMode: {
+        'hard-light': 'hard-light',
+        'plus-lighter': 'plus-lighter',
+        'color-dodge': 'color-dodge',
+      },
+    },
   },
   plugins: [],
 }
